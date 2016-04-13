d83cdd51f545c3cb7c16e16f6a6122dee18932f1
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2016-04-13 19:48:34 -0300

    First commit! :sunflower:

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clj-calculator "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-exec "0.3.6"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
