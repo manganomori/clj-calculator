2c602ddd4f2c80b65e570640040c653dcb2e566c
Brunno dos Santos <squiter85@gmail.com>
2016-04-27 20:26:04 -0300

    Uses namespaces and refactor parsers

--- core.clj
+++ core.clj
@@ -1,7 +1,10 @@
+(ns clj-calculator.core)
+
 (load-file "./parsers.clj")
+(require '[clj-calculator.parser :as parser])
 
 (defn main
   []
-  (println (call (parse-args *command-line-args*))))
+  (println (parser/call  *command-line-args*)))
 
 (main)
